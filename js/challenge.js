d74aa48a87eadce09d1843fe85fdc1b8fdc7a2c1
jrcliff <justoon7.jc@gmail.com>
2021-04-18 00:45:15 -0400

    commit tree 29e836c0e443d8955f24cccf2568061638dc158f

---
+++ js/challenge.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+})
+
+function addLike(){
+    const likeButton = document.getElementById('heart')
+    const count - document.getElementById('count')
+    
+}
